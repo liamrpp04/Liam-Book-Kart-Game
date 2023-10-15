6804872772a541d7e28fc80486652fa8b0b6f35e
liamrpp04 <liamrpp04@gmail.com>
2023-10-15 11:32:00 -0500

    Oil Obstacle

--- LiamKartGameV2/Assets/Karting/Scripts/Obstacles/OilObstacle.cs
+++ LiamKartGameV2/Assets/Karting/Scripts/Obstacles/OilObstacle.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MudSlider : MonoBehaviour
+public class OilObstacle : MonoBehaviour
 {
     [SerializeField] private TargetTriggerEnter targetTrigger;
 
